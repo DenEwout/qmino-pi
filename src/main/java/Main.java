d01793238ac21d5c04429a0ccd481174980da71c
EWOUT-QMINO\Ewout <ewout.vangossum@qmino.be>
2014-11-16 15:05:29 +0100

    Added jFileChooser code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
 import javax.sound.sampled.*;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
 
 /**
  * Created by Ewout on 16/11/2014.
@@ -14,21 +8,39 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-        // specify the sound to play
-        // (assuming the sound can be played by the audio system)
-        File soundFile = new File(Main.class.getResource("sounds/notification" +
-                ".wav").getFile());
-        AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+//        Dit werkt wel?!
+//        JFileChooser chooser = new JFileChooser();
+//        chooser.showOpenDialog(null);
+//        File audioFile = chooser.getSelectedFile();
 
-        // load the sound into memory (a Clip)
-        DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
-        Clip clip = (Clip) AudioSystem.getLine(info);
+        //Dit werkt niet
+        File audioFile = new File(Main.class.getResource
+                ("/sounds/notification.wav").getPath());
+        System.out.println(audioFile.exists());
 
-        clip.open(sound);
+        AudioInputStream ais = AudioSystem.getAudioInputStream(audioFile);
+        Line.Info lineInfo = new Line.Info(Clip.class);
+        Line line = AudioSystem.getLine(lineInfo);
+        Clip clip = (Clip) line;
+        clip.addLineListener(new LineListener() {
+            @Override
+            public void update(LineEvent event) {
+                LineEvent.Type type = event.getType();
+                if (type == LineEvent.Type.START) {
+                    System.out.println("START");
+                    clip.drain();
+                    clip.stop();
+                } else if (type == LineEvent.Type.STOP) {
+                    System.out.println("STOP");
+                    clip.close();
+                }
+            }
+        });
+        clip.open(ais);
         clip.start();
-        clip.drain();
-        clip.stop();
-        clip.close();
+
     }
 
 }
+
+
